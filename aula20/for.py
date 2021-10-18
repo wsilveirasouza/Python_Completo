74b14bdf5d1e1d25cd59955c918269c828c6fa48
wsilveirasouza <wsilveirasouza@gmail.com>
2021-10-18 09:41:59 -0300

    for else prática

--- aula20/for.py
+++ aula20/for.py
@@ -4,8 +4,8 @@ FOR ELSE
 variavel = ['joao','pedro','wellington']
 
 for valor in variavel:
+    print(valor)
     if valor.lower().startswith('j'):
         break
     else:
         print('nao existe uma palavra que começão com j.')
-        
---
+++ aula20/forelse.py
@@ -0,0 +1,13 @@
+
+'''
+FOR ELSE    
+'''
+variavel = ['wellington','matheus','lucas']
+
+for valor in variavel:
+    if valor.lower().startswith('j'):
+        pass
+        continue
+    print(valor)
+else:
+    print('nao existe uma palavra que começa com a letra j.')
