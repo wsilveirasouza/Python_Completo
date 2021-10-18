556a4f5b4ce1909abaf9aecefe72a9d1f6f04fdb
wsilveirasouza <wsilveirasouza@gmail.com>
2021-10-18 09:35:44 -0300

    estrutura for

---
+++ aula20/for.py
@@ -0,0 +1,11 @@
+'''
+FOR ELSE
+'''
+variavel = ['joao','pedro','wellington']
+
+for valor in variavel:
+    if valor.lower().startswith('j'):
+        break
+    else:
+        print('nao existe uma palavra que começão com j.')
+        
