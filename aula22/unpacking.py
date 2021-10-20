cabefe100b1adcb04342355770b8215f7543ec7d
wsilveirasouza <wsilveirasouza@gmail.com>
2021-10-20 08:28:39 -0300

    Trocando valor de variaveis

--- aula22/unpacking.py
+++ aula22/unpacking.py
@@ -2,7 +2,7 @@
 Desempacomento de listas
 '''
 lista = ['Luiz','Joao','Maria',1,2,3,4,5,6,7,8,9,100]
-
+# *_ significa que não será utilizado no restante do código
 n1, n2, n3, *outra_lista, ultimo_da_lista = lista
 
 print(n1, n2, n3, outra_lista, ultimo_da_lista)
---
+++ aula23/trocavlr.py
@@ -0,0 +1,9 @@
+'''
+Trocando valor de variavel
+'''
+x = 10
+y = 'Well'
+z = 'Jana'
+x, y, z = y, z, x
+
+print(f'x={x} e y={y} e z={z}')
