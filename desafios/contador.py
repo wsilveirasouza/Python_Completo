9ec8c979a4fe2832b9c6f153026c1936b667579d
wsilveirasouza <wsilveirasouza@gmail.com>
2021-10-20 09:20:26 -0300

    Desafio contadores simultâneos

---
+++ desafios/contador.py
@@ -0,0 +1,15 @@
+'''
+Desafio de contadores
+FOR / WHILE
+Criar 2 contadores simultâneos
+sendo que um irá contador progressivamente 
+enquanto o outro regressivamente
+* 0 a 8 - 10 a 2
+'''
+contP = 0
+contR = 10
+
+while contP <= 8:
+    print(contP, contR)
+    contP = contP + 1
+    contR = contR - 1
