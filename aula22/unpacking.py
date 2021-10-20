497a4ea14704cd8ced3ce01029c62d174406d588
wsilveirasouza <wsilveirasouza@gmail.com>
2021-10-20 08:20:25 -0300

    Desempacomento de listas

---
+++ aula22/unpacking.py
@@ -0,0 +1,8 @@
+'''
+Desempacomento de listas
+'''
+lista = ['Luiz','Joao','Maria',1,2,3,4,5,6,7,8,9,100]
+
+n1, n2, n3, *outra_lista, ultimo_da_lista = lista
+
+print(n1, n2, n3, outra_lista, ultimo_da_lista)
