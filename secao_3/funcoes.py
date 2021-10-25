ac4c6074e62d113698708c8856adeace4b483f7d
wsilveirasouza <wsilveirasouza@gmail.com>
2021-10-24 23:18:03 -0300

    Funções

---
+++ secao_3/funcoes.py
@@ -0,0 +1,9 @@
+'''
+Funções - def em Python (parte 1)
+'''
+def funcao(msg='Olá', nome='usuário'):
+    print(msg, nome)
+
+funcao('Olá','Wellington')
+funcao(nome='World')
+funcao(nome='Jackson', msg='Bye!')
